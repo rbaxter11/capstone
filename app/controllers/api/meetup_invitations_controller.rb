bc75608385b9fed6906b14ffb1cc73281da426e4
ryan <ryanandrewbaxter@gmail.com>
2021-01-14 00:10:52 -0600

    Updates Invitation Update, various partials

--- app/controllers/api/meetup_invitations_controller.rb
+++ app/controllers/api/meetup_invitations_controller.rb
@@ -15,10 +15,18 @@ class Api::MeetupInvitationsController < ApplicationController
   def update
     input = params["id"]
     @invitation = MeetupInvitation.find_by(id: input)
-    @invitation.meetup_id = params["meetup_id"] || @invitation.meetup_id
-    @invitation.user_id = params["user_id"] || @invitation.user_id
-    @invitation.invited_by_id = params["invited_by_id"] || @invitation.invited_by_id
-    @invitation.accepted = params["accepted"] || @invitation.accepted
+    @invitation.update(meetup_invitation_params)
+    # @invitation.meetup_id = params["meetup_id"] || @invitation.meetup_id
+    # @invitation.user_id = params["user_id"] || @invitation.user_id
+    # @invitation.invited_by_id = params["invited_by_id"] || @invitation.invited_by_id
+    # Rails.logger.info("-------------------------------")
+    # Rails.logger.info("-------------------------------")
+    # Rails.logger.info(params)
+    # Rails.logger.info("-------------------------------")
+    # Rails.logger.info(params["accepted"])
+    # Rails.logger.info("-------------------------------")
+    # Rails.logger.info("-------------------------------")
+    # @invitation.accepted = params["accepted"] || @invitation.accepted
     if @invitation.save
       render "show.json.jb"
     else
@@ -46,5 +54,11 @@ class Api::MeetupInvitationsController < ApplicationController
     @invitation = MeetupInvitation.find_by(id: input)
     @invitation.destroy
     render json: { message: "Invitation deleted" }
+  end
+
+  private
+
+  def meetup_invitation_params
+    params.require(:meetup_invitation).permit(:accepted)
   end
 end
